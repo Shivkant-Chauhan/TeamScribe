4864ab93f5c13be108fd2f6c340b6f2ff5dc3419
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-04-03 14:48:32 +0530

    initializing project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Text_Editor from './components/Text_Editor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Text_Editor></Text_Editor>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Text_Editor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as Y from 'yjs';
+
+function Text_Editor() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Text_Editor;
