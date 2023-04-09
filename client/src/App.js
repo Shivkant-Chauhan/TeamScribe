db74d117afe99253d5f781183b5dcea90af135f4
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-04-09 06:14:02 +0530

    added functional quill text editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import TextEditor from './components/TextEditor.jsx';
-import './App.css';
+import { v4 as uuidV4 } from 'uuid';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TextEditor></TextEditor>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Navigate to={`/documents/${uuidV4()}`} />} />
+          <Route exact path='/documents/:id' element={<TextEditor />} />
+
+          {/* <Route path="*" element={<Error />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
